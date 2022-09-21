f56869a11c65062dc550bb75b7420224963f1367
Wahyu Kristiyanto <wahyukristiyanto24@gmail.com>
2022-09-21 09:44:38 +0700

    Application Environment

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "name" => [
+        "first" => env('NAME_FIRST','Rian'),
+        "last" => env('NAME_LAST','nvs')
+    ],
+    "email" => "rian@mail.com",
+    "web" => "https://rian.com"
+];
---
+++ tests/Feature/AppEnvirontmentTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
+use Tests\TestCase;
+
+class AppEnvirontmentTest extends TestCase
+{
+    public function testAppEnv(){
+        if(App::environment(['testing','prod','dev'])){
+            self::assertTrue(true);
+        }
+    }
+}
